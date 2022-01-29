d1c713c736c81fbab2b47879c98a704b83d3a2e4
Jugad Khajuria <curaww@gmail.com>
2022-01-29 03:03:23 -0800

    [add] logic for decoding a 6xkk load opcode
    Program can now decode 6xkk where we load value kk into register x

--- main.c
+++ main.c
@@ -1,12 +1,41 @@
+#include "main.h"
 #include <SDL2/SDL.h>
 #include <stdbool.h>
 #include <stdio.h>
 
-int main() {
+unsigned char memory[4096];
+int display[64][32];
+unsigned char registers[16];
+unsigned char sound_register;
+bool play_sound;
+unsigned char delay_register;
+bool delay_not_zero;
+int program_counter;
+int stack[16];
+int stack_pointer;
 
-  unsigned char memory[4096];
-  int display[64][32];
-  int registers[12];
+// Every Opcode is a two byte value, a hex value is represented by 4 binary
+// numbers or half a byte So that means that an opcode contains 4 hex numbers,
+// these can then be decoded to figure out what opcode is being called
+
+// Ex. 6xkk, the 6 is the first nibble and shows that this opcode will take
+// value kk and place it in register x
+unsigned int current_opcode;
+
+// Each instruction is made of two bytes, to help decode them we store both
+unsigned char first_opcode_byte;
+unsigned char second_opcode_byte;
+
+// Each nibble is 4 bits, or one number in the binary file in hex
+// We can use these to decode what the instruction is
+unsigned char first_opcode_nibble;
+unsigned char second_opcode_nibble;
+unsigned char third_opcode_nibble;
+unsigned char fourth_opcode_nibble;
+
+int main(int argc, char **argv) {
+
+  stack_pointer = &stack[0];
 
   SDL_Renderer *renderer = NULL;
   SDL_Event event;
@@ -33,7 +62,7 @@ int main() {
   fread(&memory[512], sizeof(memory), 1, fp);
 
   int current_address = 512;
-
+  bool read_rom = false;
 
   while (!quit) {
 
@@ -41,13 +70,36 @@ int main() {
 
     SDL_RenderSetScale(renderer, 8, 8);
 
-    if(current_address > 520) {
+    if (current_address > 514 && !read_rom) {
+
+      read_rom = false;
+
+    } else {
+
+      first_opcode_byte = memory[current_address];
+      second_opcode_byte = memory[current_address + 1];
+
+      current_opcode =
+          (memory[current_address] * 0x100) + memory[current_address + 1];
+
+      // Decode opcode
+
+      first_opcode_nibble = current_opcode / 0x1000;
+      second_opcode_nibble = (current_opcode / 0x100) % 0x10;
+      third_opcode_nibble = (current_opcode / 0x10) % 0x10;
+      fourth_opcode_nibble = current_opcode % 0x10;
+
+      if (first_opcode_nibble == 0x6) {
+
+        registers[second_opcode_nibble] = second_opcode_byte;
+      }
+
+      print_debug_info();
       quit = true;
       break;
+
+      current_address = current_address + 2;
     }
-
-    printf("%x%x", memory[current_address], memory[current_address+1]);
-
 
     for (int i = 0; i < 64; i++) {
       for (int j = 0; j < 32; j++) {
@@ -72,15 +124,31 @@ int main() {
     // Checks if close button is pressed and exits the program
     switch (event.type) {
     case SDL_QUIT:
+      print_debug_info();
       quit = true;
       break;
     }
-
-    current_address++;
   }
 
   fclose(fp);
-  SDL_Quit();
+  //  SDL_Quit();
 
   return 0;
+}
+
+void print_debug_info() {
+
+  printf("\n");
+
+  printf("current opcode %x\n", current_opcode);
+  printf("opcode first byte %x\n", first_opcode_byte);
+  printf("opcode second byte %x\n", second_opcode_byte);
+  printf("opcode first nibble %x\n", first_opcode_nibble);
+  printf("opcode second nibble %x\n", second_opcode_nibble);
+  printf("opcode third nibble %x\n", third_opcode_nibble);
+  printf("opcode fourth nibble %x\n", fourth_opcode_nibble);
+  for (int current_register = 0; current_register < 16; current_register++) {
+
+    printf("register%x: %x\n", current_register, registers[current_register]);
+  }
 }
---
+++ main.h
@@ -0,0 +1,2 @@
+void print_debug_info();
+
